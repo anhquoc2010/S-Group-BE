16870592f524d1087564e45cfb222fecd58b1a44
anhquoc2010 <muicamau007@gmail.com>
2023-04-13 21:31:54 +0700

    feat: config .env

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,11 +1,13 @@
 import mysql from 'mysql'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const connection = mysql.createPool({
     connectionLimit: 10,
-    host: 'localhost',
-    user: 'root',
-    password: '123456',
-    database: 'sgroup_be'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 })
 
 connection.getConnection((err) => {
